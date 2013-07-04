9df5a20bdb88fcb7e0f7be5bcd3b6505d37a1c9a
Andrew Gibson <andrew@goofballLogic.com>
2013-07-04 09:31:18 +0100

    Reset for v0.3

--- spec/buster.js
+++ spec/buster.js
@@ -9,23 +9,3 @@ config["main"] = {
 		"spec/*-spec.js"
 	]
 };
-
-config["riak"] = {
-	rootPath: "../",
-	environment: "node",
-	sources: [
-	],
-	tests: [
-		"spec-integration/repo-riak*-spec.js"
-	]
-};
-
-config["express"] = {
-	rootPath: "../",
-	environment: "node",
-	sources: [
-	],
-	tests: [
-		"spec-integration/adapter-express*-spec.js"
-	]
-};
